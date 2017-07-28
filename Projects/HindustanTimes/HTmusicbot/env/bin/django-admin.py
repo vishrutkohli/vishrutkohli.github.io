afcb07fdef643c433beb6b968fd8fdfa83a1b236
vishrutkohli <kohlivishrut@gmail.com>
2017-07-29 04:57:22 +0530

    added internship projects

---
+++ Projects/HindustanTimes/HTmusicbot/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vishrut/Desktop/Projects/HindustanTimes/HTmusicbot/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Projects/HindustanTimes/envhatecrime/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vishrut/Desktop/Projects/HindustanTimes/envhatecrime/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
